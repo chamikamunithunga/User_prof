3cd6a2abefa06cae866257d0906a3adab9e5d819
H.D Chamika Munithunga <142832796+chamikamunithunga@users.noreply.github.com>
2024-09-07 23:08:06 +0530

    Add files via upload

---
+++ prof.js
@@ -0,0 +1,11 @@
+document.querySelector('.back-button').addEventListener('click', function() {
+    window.history.back();
+});
+
+document.querySelector('.edit-button').addEventListener('click', function() {
+    alert('Edit profile functionality will go here.');
+});
+
+document.querySelector('.change-button').addEventListener('click', function() {
+    alert('Change functionality will go here.');
+});
